df60c18497c02f39777ae5fc9e319b81d31405dd
isirollan <isirollan@gmail.com>
2024-03-20 16:19:12 -0700

    CapstoneProject

---
+++ src/amplify-theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from '@aws-amplify/ui';
+
+const amplifyTheme = createTheme({
+  // Define your custom theme variables here
+  // For example:
+  // colors: {
+  //   primary: 'blue',
+  //   secondary: 'green',
+  //   ...
+  // }
+});
+
+export default amplifyTheme;
