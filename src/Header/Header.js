e1c549af24a51e772c4091881ba5668d0e82b2ae
isirollan <isirollan@gmail.com>
2024-05-02 18:39:17 -0700

    Navigate to home when pressing the logo

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,7 @@
 import '../styles/header.css';
 import React from 'react';
 import { signOut } from 'aws-amplify/auth';
+import { useNavigate } from "react-router";
 
 // Amplify signout function
 async function handleSignOut() {
@@ -11,13 +12,21 @@ async function handleSignOut() {
   }
 }
 
+
+
 function Header() {
+  //navigate to Home when pressing the logo
+  const navigate = useNavigate(); 
+  const homeClick = (e) => {
+    e.preventDefault();
+    navigate("/")
+}
     return (
         <>
             <div className='header'>
                 <div className='left'></div>
                 <div className='middle'>
-                    <p className='logo'>r.</p>
+                    <p className='logo' onClick={homeClick}>r.</p>
                 </div>
                 <div className='right'>
                   <button className='signoutbutton' onClick={handleSignOut}>Sign Out</button>
